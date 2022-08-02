cbd7cf4f452aa5766627553f07923a31110dcaed
Fahim59 <mrahaman59@yahoo.com>
2022-08-02 11:12:18 +0600

    Screenshot listener added

---
+++ src/test/java/com/RDCD/Screenshot.java
@@ -0,0 +1,34 @@
+package com.RDCD;
+
+import com.BaseClass.*;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+import java.io.IOException;
+
+public class Screenshot extends BaseClass implements ITestListener {
+
+    @Override
+    public void onTestFailure(ITestResult result) {
+        System.out.println("Test Failed");
+
+        try{
+            Screenshot(result.getMethod().getMethodName()); //Taken from BaseClass
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onTestSkipped(ITestResult result) {
+        System.out.println("Test Skipped");
+
+        try{
+            Screenshot(result.getMethod().getMethodName()); //Taken from BaseClass
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
